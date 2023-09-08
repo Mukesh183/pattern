4c59e69f00cf9a79f2139437df80ef072b073a33
Mukesh <67510024+Mukesh183@users.noreply.github.com>
2023-09-08 19:39:10 +0530

    Update learn.py

--- learn.py
+++ learn.py
@@ -1,3 +1,4 @@
 print("Hi all im leaening git not python as of now thank you")
 print("Hi, Im modifying this")
 print("If you pull this you can see this in your system")
+print("Today is 8/9/2023 I will get a job as soon as possible")
